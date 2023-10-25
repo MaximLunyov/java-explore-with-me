ab9e14dfeb46413c40ba62ffd4f854878614274c
MaximLunyov <Mr.Minnimi@yandex.com>
2023-10-25 11:23:51 +0300

    "feat: Проверка прохождения тестов на GitHub 4, добавлена обработка ошибок на StatsServer"

---
+++ stats/stats-service/src/main/java/ru/practicum/stats/server/error/StatsErrorMessage.java
@@ -0,0 +1,10 @@
+package ru.practicum.stats.server.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class StatsErrorMessage {
+    private final String error;
+}
